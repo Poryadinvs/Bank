84de1934434737d001fbad35bca3a5b1d4090692
Vlad <vsporyadin@gmail.com>
2023-12-25 20:32:20 +0300

    Добавьте файлы проекта.

---
+++ Administrator/Form1.cs
@@ -0,0 +1,10 @@
+namespace Administrator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Bank'/Form1.cs
@@ -0,0 +1,10 @@
+namespace Bank_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
